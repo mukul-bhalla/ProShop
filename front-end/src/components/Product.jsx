80fba7248cfef0a4d20b2996dc8ad4f45c7555ca
mukul-bhalla <bhallamukul27@gmail.com>
2023-12-10 15:17:24 +0530

    Rating Component

--- front-end/src/components/Product.jsx
+++ front-end/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 const Product = ({ product }) => {
     return (<>
         <Card className='my-3 p-3 rounded'>
@@ -9,15 +10,18 @@ const Product = ({ product }) => {
 
             <Card.Body>
                 <Link to={`product/${product._id}`}>
-                    <Card.Title as="div">
-                        <strong>{product.name}</strong>
+                    <Card.Title className='product-title' as="div" >
+                        <strong >{product.name}</strong>
                     </Card.Title>
                 </Link>
+                <Card.Text as="div">
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                </Card.Text >
 
+                <Card.Text as="h3">
+                    ${product.price}
+                </Card.Text>
             </Card.Body>
-            <Card.Text as="h3">
-                ${product.price}
-            </Card.Text>
         </Card>
     </>
     )
