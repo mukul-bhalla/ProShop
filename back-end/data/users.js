b5627145a0da164babedef6362c16f724de446c9
mukul-bhalla <bhallamukul27@gmail.com>
2023-12-12 22:45:14 +0530

    Dummy data

---
+++ back-end/data/users.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false
+    },
+    {
+        name: 'Jane Doe',
+        email: 'Jane@email.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: false
+    },
+]
+module.exports = users
