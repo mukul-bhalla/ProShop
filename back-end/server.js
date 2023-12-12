084cf2273301a35db6744712dd8e024bd934587b
mukul-bhalla <bhallamukul27@gmail.com>
2023-12-12 19:25:36 +0530

    Revert "This commit is a mistake"
    This reverts commit b187c096ac0d2772a332e4ed89595e162853ec7e.

--- back-end/server.js
+++ back-end/server.js
@@ -1,18 +1,16 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const app = express();
-dotenv.config();
 const products = require('./data/products')
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.get('/', (req, res) => {
     res.send('API is running');
 })
 
-app.get('/api/products', (req, res) => {
+app.get('/products', (req, res) => {
     res.json(products)
 })
 
-app.get('/api/products/:id', (req, res) => {
+app.get('/products/:id', (req, res) => {
     const { id } = req.params;
     const p = products.map((p) => {
 
