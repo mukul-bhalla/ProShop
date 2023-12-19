5ea757dd17f62c973b8e42acda616eb35f25be89
mukul-bhalla <bhallamukul27@gmail.com>
2023-12-19 22:51:16 +0530

    error middleware

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
+
 const app = express();
+
+const notFound = require('./middleware/errorMiddleware')
+const errorHandler = require('./middleware/errorMiddleware')
 
 const productRoutes = require('./Routes/productRoutes');
 
@@ -9,12 +13,17 @@ const port = process.env.PORT || 5000;
 const Cors = require('cors');
 const connectDB = require('./config/db')
 connectDB();
+
 app.use(Cors());
+
 app.get('/', (req, res) => {
     res.send('API is running');
 })
 
 app.use('/api/products', productRoutes);
+
+// app.use(notFound);
+// app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Listening at PORT ${port}`)
