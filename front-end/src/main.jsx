3ac52a8d012c88b00fb38f317bef743a562d7b94
mukul-bhalla <bhallamukul27@gmail.com>
2024-05-28 19:32:27 +0530

    Cart Functionality Complete

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -13,12 +13,15 @@ import store from './store';
 import './assets/styles/bootstrap.custom.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import ProductScreen from './screens/ProductScreen.jsx'
+import CartScreen from './screens/CartScreen.jsx'
+
 // import './index.css'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />}></Route>
       <Route path='/product/:id' element={<ProductScreen />}></Route>
+      <Route path='/cart' element={<CartScreen />}></Route>
     </Route>
   )
 )
