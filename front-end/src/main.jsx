0384d340cb44ea3c6f05e59f4aa4316ca7e8e3ab
mukul-bhalla <bhallamukul27@gmail.com>
2024-06-03 22:14:53 +0530

    Auth Slice and LoginScreen.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -14,6 +14,7 @@ import './assets/styles/bootstrap.custom.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import ProductScreen from './screens/ProductScreen.jsx'
 import CartScreen from './screens/CartScreen.jsx'
+import LoginScreen from './screens/LoginScreen.jsx'
 
 // import './index.css'
 const router = createBrowserRouter(
@@ -22,6 +23,7 @@ const router = createBrowserRouter(
       <Route index={true} path='/' element={<HomeScreen />}></Route>
       <Route path='/product/:id' element={<ProductScreen />}></Route>
       <Route path='/cart' element={<CartScreen />}></Route>
+      <Route path='/login' element={<LoginScreen />}></Route>
     </Route>
   )
 )
